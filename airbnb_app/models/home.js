2c74d3d7702e1ad40a708133302003b8f6687547
Nimish Agarwal <agarwalnimish428@gmail.com>
2025-10-05 06:39:30 +0530

    updated

--- airbnb_app/models/home.js
+++ airbnb_app/models/home.js
@@ -1,17 +1,23 @@
-const mongoose=require("mongoose");
+const mongoose = require("mongoose");
 const favourite = require("./favourite");
 
-const homeSchema= new mongoose.Schema({
- houseName:{type:String,required:true},
- price:{type:Number,required:true},
- location:{type:String,required:true},
- rating:{type:Number,required:true},
- photoUrl:String
+const homeSchema = new mongoose.Schema({
+  houseName: { type: String, required: true },
+  price: { type: Number, required: true },
+  location: { type: String, required: true },
+  rating: { type: Number, required: true },
+  photoUrl: String,
+  user: { 
+    type: mongoose.Schema.Types.ObjectId, 
+    ref: 'User', 
+    required: true 
+  }
 });
 
-homeSchema.pre('findOneAndDelete',async function(next){
-   const homeId=this.getQuery()._id;
-   await favourite.deleteMany({homeId:homeId});
-   next();
+homeSchema.pre('findOneAndDelete', async function(next) {
+  const homeId = this.getQuery()._id;
+  await favourite.deleteMany({ homeId: homeId });
+  next();
 });
-module.exports=mongoose.model('Home',homeSchema);
+
+module.exports = mongoose.model('Home', homeSchema);
